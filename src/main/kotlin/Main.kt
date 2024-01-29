5cb011c3fbaefa29845356d40a2c88d9e0825f55
Gizatulin-Vitaly <my@vgmail.ru>
2024-01-29 19:55:33 +0500

    Gizatulin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+import parser.CsvParser
+import resolver.Resolver
+
 fun main(args: Array<String>) {
-    print("Yeah rock!")
+    val players = CsvParser.parse("src/main/resources/fakePlayers.csv")
+    val resolver = Resolver(players)
+    println(resolver.getCountWithoutAgency())
+    println(resolver.getBestScorerDefender())
+    println(resolver.getTheExpensiveGermanPlayerPosition())
+    println(resolver.getTheRudestTeam())
 }
--- src/main/kotlin/resolver/Resolver.kt
+++ src/main/kotlin/resolver/Resolver.kt
@@ -1,3 +1,41 @@
 package resolver
 
-class Resolver
+import model.Player
+import model.Position
+import model.Team
+import kotlin.reflect.KClass
+
+class Resolver(private val players: List<Player>): IResolver {
+
+    override fun getCountWithoutAgency(): Int =
+        players.count { it.agency.isEmpty() }
+
+
+    override fun getBestScorerDefender(): Pair<String, Int> =
+        players
+            .filter { it.position == Position.DEFENDER }
+            .maxBy { it.goalsCount }
+            .let { it.name to it.goalsCount }
+
+    override fun getTheExpensiveGermanPlayerPosition() =
+        players
+            .filter { it.nationality == "Germany" }
+            .maxBy { it.goalsCount }
+            .let {
+                when (it.position) {
+                    Position.DEFENDER -> "Защитник"
+                    Position.GOALKEEPER -> "Вратарь"
+                    Position.MIDFIELD -> "Полузащитник"
+                    Position.FORWARD -> "Нападающий"
+                    else -> ""
+                }
+            }
+
+    //Выберите команду с наибольшим числом удалений на одного игрока
+    // Выберите команду с наибольшим средним числом удалений на одного игрока.
+    override fun getTheRudestTeam(): Team {
+        return players
+            .groupBy { it.team }
+            .maxBy { it.value.sumOf { player -> player.redCardsCount } / it.value.size }.key
+    }
+}
